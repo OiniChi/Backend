8c57256c355fb7fa30342c170e350c99db27270e
Backend_Pract <andreyka.lysenko.2002@mail.ru>
2024-01-22 18:56:33 +0700

    First commit 2

--- main.py
+++ main.py
@@ -17,7 +17,3 @@ async def print_func() -> str:
 @app.get('/users')
 async def print_func() -> str:
     return "Номер тел. : +79234671287"
-
-@app.get('/tools2')
-async def print_func() -> str:
-    return f"Навыки: Владения ООП, базовое владение реляционными базами данных SQL/PostgreSQL, знания языков: С/C++/C#, Python, знания фреймворков .Net Framework, FastAPI, Docker..."
