e588e8273142e50998f2f189f2addb96a19b6287
Backend_Pract <andreyka.lysenko.2002@mail.ru>
2024-01-22 20:00:39 +0700

    First commit

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def print_func() -> str:
+async def print_func1() -> str:
     return "Лысенко Андрей Максимович"
 
 @app.get('/users')
-async def print_func() -> str:
+async def print_func2() -> str:
     return "Номер тел. : +79234671287"
 
 @app.get('/tools')
-async def print_func() -> str:
+async def print_func3() -> str:
     return f"Навыки: Владения ООП, базовое владение реляционными базами данных SQL/PostgreSQL, знания языков: С/C++/C#, Python, знания фреймворков .Net Framework, FastAPI, Docker..."
 
 @app.get('/users')
-async def print_func() -> str:
+async def print_func4() -> str:
     return "Номер тел. : +79234671287"
